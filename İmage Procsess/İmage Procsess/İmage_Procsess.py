d289981ace6747eab23fd2ca62f68096fa702589
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-08-25 00:43:01 +0300

    Delete İmage Procsess directory

--- İmage Procsess/İmage Procsess/İmage_Procsess.py
+++
@@ -1,4 +0,0 @@
-import cv2
-
-#Burada imread komutu resimi oku anlamýna geliyor. Ayrýca bu komut çaðýrýlýrken kendi kütüphanesini yani cv2 kütüphanesini de baþýna alýyor.
-resim = cv2.imread("photo.jpg")
