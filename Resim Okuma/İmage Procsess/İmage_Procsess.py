9b6bfe9b191cefc6459e40d1e3eb79d8291615ea
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-08-25 00:46:08 +0300

    Add files via upload

---
+++ Resim Okuma/İmage Procsess/İmage_Procsess.py
@@ -0,0 +1,4 @@
+import cv2
+
+#Burada imread komutu resimi oku anlamýna geliyor. Ayrýca bu komut çaðýrýlýrken kendi kütüphanesini yani cv2 kütüphanesini de baþýna alýyor.
+resim = cv2.imread("photo.jpg")
